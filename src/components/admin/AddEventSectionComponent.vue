802009b1de21cff735989a461b97c3fb58044f35
Malkollm <malkollm.mob.projects@gmail.com>
2023-02-09 21:24:37 +0500

    Начал разбираться с добавлением файлов

--- src/components/admin/AddEventSectionComponent.vue
+++ src/components/admin/AddEventSectionComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <admin-tab-component />
+  <admin-tab-component/>
   <div class="container w-25 p-3">
     <form @submit.prevent="handleSubmit">
       <div class="form-floating mb-3">
@@ -11,29 +11,15 @@
                   style="height: 100px"></textarea>
         <label for="floatingTextarea2">Описание новости</label>
       </div>
+      <div class="mb-3">
+        <input class="form-control" id="files" ref="files" type="file" @change="handleFileInput" multiple>
+      </div>
       <div class="form-check mb-5">
         <input v-model="data.isPublished" class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
         <label class="form-check-label" for="flexCheckDefault">
           Публиковать новость?
         </label>
       </div>
-
-      <div
-          class="upload upload-picture-card"
-          @click="triggerFileInputDialog"
-      >
-        <span style="display: none">
-      <input
-          id="image_upload"
-          accept="image/*"
-          multiple
-          name="image_upload"
-          type="file"
-          @change="handleFileInput"
-      >
-    </span>
-      </div>
-
       <button type="submit" class="btn btn-primary">Сохранить</button>
       <!--      <p>{{ JSON.stringify(data, null, 2) }}</p>-->
     </form>
@@ -48,11 +34,32 @@ import AdminTabComponent from "@/components/admin/AdminTabComponent";
 export default {
   components: {AdminTabComponent},
   name: 'AdminComponent',
+  data() {
+    return {
+      files: ''
+    }
+  },
   setup() {
     const data = reactive({title: "", text: "", isPublished: false, files: []})
 
-    function triggerFileInputDialog() {
-      document.getElementById('image_upload')?.click();
+    function handleSubmit() {
+      const formData = new FormData();
+
+      formData.append('title', data.title);
+      formData.append('content', data.text);
+      formData.append('isPublished', data.isPublished);
+      formData.append('isPublished', data.files);
+
+      axios
+          .post('http://api.malkollm.ru/Events/', formData,
+          ).then((response) => {
+        console.log(response.data)
+        console.log('SUCCESS!!')
+      })
+          .catch((error) => {
+            console.log(error)
+            console.log('FAILURE!!')
+          })
     }
 
     function handleFileInput(e) {
@@ -77,22 +84,7 @@ export default {
       data.files.push(...result)
     }
 
-    function handleSubmit() {
-      const formData = new FormData();
-      formData.append('EventId', data.title);
-      formData.append('File', data.files);
-
-      axios
-          .post('http://api.malkollm.ru/News/', formData)
-          .then((response) => {
-            console.log(response.data)
-          })
-          .catch((error) => {
-            console.log(error)
-          })
-    }
-
-    return {triggerFileInputDialog, handleFileInput, data, handleSubmit}
+    return {handleFileInput, data, handleSubmit}
   }
 }
 </script>
