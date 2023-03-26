e987cd3fc4e15994c7ed732fdc2e1cb74fca0a20
Malkollm <malkollm.mob.projects@gmail.com>
2023-03-26 23:45:49 +0500

    Готовый вариант расписания

--- src/components/ScheduleComponent.vue
+++ src/components/ScheduleComponent.vue
@@ -5,40 +5,44 @@
       <div class="col-2"></div>
       <div class="card mb-3 col-8">
         <form @submit.prevent="handleSubmit">
-          <div class="col-12 mb-4">
-            <label class="" for="inlineFormSelectPref">Выберите дату</label>
-            <select class="form-select" id="inlineFormSelectPref" v-model="data.date">
-              <option v-for="date in dates" :value="date.date" :key="date.id">
-                {{ date.date }} {{ getDayOfWeek(date.date) }}
-              </option>
-            </select>
+          <div class="row">
+            <div class="col-8 mb-4">
+              <label class="" for="inlineFormSelectPref">Выберите дату</label>
+              <select class="form-select" id="inlineFormSelectPref" v-model="this.datew">
+                <option v-for="date in dates" :value="date.date" :key="date.id">
+                  {{ getFormatDate(date.date) }} {{ getDayOfWeek(date.date) }}
+                </option>
+              </select>
+            </div>
+            <div class="col-4 mb-4">
+              <button type="submit" class="btn btn-primary mt-4">Посмотреть</button>
+            </div>
           </div>
-          <button type="submit" class="btn btn-primary">Посмотреть</button>
-          <table class="table">
-            <thead>
-            <tr>
-              <th scope="col">Номер класса</th>
-              <th scope="col">Дисциплина</th>
-              <th scope="col">ФИО преподавателя</th>
-              <th scope="col">Электронная почта</th>
-            </tr>
-            </thead>
-
-            <tbody v-for="lesson in dates.lessons" :key="lesson.id">
-            <tr colspan="2" >
-              <td v-for="value in item" :key="value.id">
+        </form>
+        <h3>{{ this.currentDate.date }}</h3>
+        <table class="table">
+          <thead>
+          <tr>
+            <th scope="col">Номер класса</th>
+            <th scope="col">Дисциплина</th>
+            <th scope="col">ФИО преподавателя</th>
+          </tr>
+          </thead>
+          <tbody>
+          <template v-for="value in lessons" :key="value.id">
+            <tr colspan="2">
+              <td>
                 <strong>{{ value.numberGroup }} класс</strong>
               </td>
             </tr>
-            <tr colspan="2" v-for="value in lesson.lessonItems" :key="value.id">
-              <td>{{ `${value.lessonStart} - ${value.lessonEnd}` }}</td>
-              <td>{{ value.lessonName }}</td>
-              <td>{{ `${value.lastName} ${value.firstName} ${value.middleName}` }}</td>
-              <td>{{ value.email }}</td>
+            <tr v-for="item in value.lessonItems" :key="item.id">
+              <td>{{ `${item.lessonStart} - ${item.lessonEnd}` }}</td>
+              <td>{{ item.lessonName }}</td>
+              <td>{{ `${item.lastName} ${item.firstName} ${item.middleName}` }}</td>
             </tr>
-            </tbody>
-          </table>
-        </form>
+          </template>
+          </tbody>
+        </table>
       </div>
       <div class="col-2"></div>
     </div>
@@ -47,7 +51,6 @@
 </template>
 
 <script>
-import {reactive} from "vue";
 import axios from 'axios'
 import HeaderComponent from './static/HeaderComponent'
 import FooterComponent from "@/components/static/FooterComponent";
@@ -61,11 +64,12 @@ export default {
       uniqueDates: [],
       lessons: [],
       currentDate: "",
+      datew: "",
     }
   },
   mounted() {
     axios
-        .get('https://localhost:7276/ScheduleDates')
+        .get('http://api.malkollm.ru/ScheduleDates')
         .then((response) => {
           this.dates = response.data
         })
@@ -74,41 +78,56 @@ export default {
         })
 
     axios
-        .get('http://api.malkollm.ru/ScheduleDates/GetScheduleByDate')
+        .get('http://api.malkollm.ru/ScheduleDates/GetScheduleByDate', {
+          params: {
+            date: this.getCurrentDate()
+          }
+        })
         .then((response) => {
-          console.log(response.data)
-          this.lessons = response.data
+          this.currentDate = response.data
+          this.lessons = response.data.lessons
+          console.log(this.currentDate)
         })
         .catch((error) => {
           console.log(error)
         })
   },
-  setup() {
-    const data = reactive({date: ""})
-
-    function handleSubmit() {
-      axios
-          .get('https://localhost:7276/ScheduleDates/GetScheduleByDate', {
-            params: {
-              date: data.date
-            }
-          })
-          .then((response) => {
-            this.uniqueDates = response.data
-            console.log(this.uniqueDates)
-          })
-          .catch((error) => {
-            console.log(error)
-          })
-    }
-
-    return {data, handleSubmit}
-  },
   methods: {
+    getCurrentDate() {
+      const date = new Date();
+
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+
+      return `${year}-0${month}-${day}T00:00:00`;
+    },
+
     getDayOfWeek(date) {
       const dayOfWeek = new Date(date).getDay();
       return isNaN(dayOfWeek) ? null :
           ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'][dayOfWeek];
+    },
+
+    getFormatDate(date) {
+      return new Date(date).toISOString().split('T', 1)[0]
+    },
+
+    handleSubmit() {
+      axios
+          .get('http://api.malkollm.ru/ScheduleDates/GetScheduleByDate', {
+            params: {
+              date: this.datew
+            }
+          })
+          .then((response) => {
+            this.currentDate = response.data
+            this.lessons = response.data.lessons
+            console.log(this.currentDate)
+          })
+          .catch((error) => {
+            console.log(error)
+          })
     }
   }
 }
